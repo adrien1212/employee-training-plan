5d9243185f93d4669eea0f12b195e5704fbf680f
adrien1212 <batman.caubel@laposte.net>
2025-07-14 09:43:59 +0200

    development config

--- backend/gateway/src/main/java/fr/adriencaubel/etp/gateway/CustomCorsConfiguration.java
+++ backend/gateway/src/main/java/fr/adriencaubel/etp/gateway/CustomCorsConfiguration.java
@@ -12,9 +12,14 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080"));
-        config.setAllowedOrigins(List.of("http://localhost", "http://127.0.0.1"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:8080",
+                "http://127.0.0.1:8080",
+                "http://localhost",
+                "http://127.0.0.1"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         return config;
