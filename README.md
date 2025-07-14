1b7c47531aed498e73b81ca4214f356d4da13359
adrien1212 <batman.caubel@laposte.net>
2025-07-14 09:23:23 +0200

    run with docker

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Backend  : /backend
-Frontend : /frontend
+- Backend  : /backend
+- Frontend : /frontend
+
+## Run the app
+- In `/etc/hosts` add `127.0.0.1 keycloak`
+- Run `docker compose up`
+- Go to `http://localhost:80/sign-up`
--- frontend/src/Keycloak.ts
+++ frontend/src/Keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url:   "http://host.docker.internal:7980",
+  url:   "http://keycloak:7980",
   realm: 'employee-training-plan',
   clientId: 'employee-training-plan-frontend-client',
 });
